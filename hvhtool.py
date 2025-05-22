a2fa40ec1bed3ab471fe5604d2b0a08691bfd433
HoàngHuy <138868537+shopaccrandom@users.noreply.github.com>
2025-05-22 22:17:45 +0700

    Update hvhtool.py

--- hvhtool.py
+++ hvhtool.py
@@ -181,6 +181,7 @@ print("\033[1;37m╚════════════════════
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 1 \033[1;31m] \033[1;32mTool GOLIKE AutoLinkedin\033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 2 \033[1;31m] \033[1;32mTool GOLIKE INSTAGRAM \033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 3 \033[1;31m] \033[1;32mTool GOLIKE INSTAGRAM RANDOM User_Agent \033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
+print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 15 \033[1;31m] \033[1;32mTool GOLIKE X \033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
 #print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 3 \033[1;31m] \033[1;32mTool TDS FB vip ")
 print("\033[1;31m────────────────────────────────────────────────────────────")
 print("\033[1;37m╔═══════════════════════╗")
@@ -247,6 +248,8 @@ if chon == 13 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVHsdfgbvusjygfuierfhsfvbskugahzusebtfgaiusetbgvasitdcziubv5465argedrsardhtdudtyregzsergsrfyu/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL%20TI%E1%BB%86N%20%C3%8DCH%20FACEBOOK/GET_NOIDUNGFB.py').text)
 if chon == 14 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVHsdfgbvusjygfuierfhsfvbskugahzusebtfgaiusetbgvasitdcziubv5465argedrsardhtdudtyregzsergsrfyu/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL%20TI%E1%BB%86N%20%C3%8DCH%20FACEBOOK/%5B21%5D%20Tool%20Reg%20Page%20Pro5.py').text)
+if chon == 15 :
+  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVHsdfgbvusjygfuierfhsfvbskugahzusebtfgaiusetbgvasitdcziubv5465argedrsardhtdudtyregzsergsrfyu/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/X/AUTO-X_PC.py').text)
 if chon == 00 :
   exit()
 
