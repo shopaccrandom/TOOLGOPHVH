1c989f4d8dba4ff3cd55dc068ad50db1fbbbf951
HoàngHuy <138868537+shopaccrandom@users.noreply.github.com>
2025-06-11 19:37:33 +0700

    Update hvhtool.py

--- hvhtool.py
+++ hvhtool.py
@@ -28,6 +28,7 @@ banner1=f'''
 \033[1;32m║ * Tool chạy trên cmd,codespace,teramux(ubuntu) (teramux thường không chạy đc)
 \033[1;32m║ * Về Phía lỗi tool thì tool đẫ được test trước khi phát hành 
 \033[1;32m║ * Nếu có lỗi thì liên hệ AD hoặc cài lại python đúng phiên bản
+\033[1;32m║ * tool reg toktok có căn mới chạy đc
 \033[1;39m└──────────────────────────────────────────────────────────────┘'''
 banner = f""" 
 \033[0;31m██╗░░██╗██╗░░░██╗██╗░░██╗  ████████╗░█████╗░░█████╗░██╗░░░░░
@@ -110,14 +111,12 @@ print("\033[1;31m─────────────────────
 print("\033[1;37m╔═════════════════════╗")
 print("\033[1;37m║ \033[1;33m GOLIKE PC|IOS|ANR\033[1;37m  ║")
 print("\033[1;37m╚═════════════════════╝")
-print("\033[1;31m[\033[1;37m_VUI LÒNG TẢI TỆP User_Agent.txt TRONG NHÓM DISCOER TRƯỚC KHI CHẠY GOLIKE INSTAGRAM RANDOM User_Agent\033[1;31m]_")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 1 \033[1;31m] \033[1;32mTool GOLIKE AutoLinkedin\033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
-print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 2 \033[1;31m] \033[1;32mTool GOLIKE INSTAGRAM \033[1;31m[\033[1;33m PC|CODESPACES\033[1;31m]")
-print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 3 \033[1;31m] \033[1;32mTool GOLIKE INSTAGRAM RANDOM User_Agent \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
+print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 3 \033[1;31m] \033[1;32mTool GOLIKE INSTAGRAM AUTO RANDOM User_Agent \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 4 \033[1;31m] \033[1;32mTool GOLIKE X \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 5 \033[1;31m] \033[1;32mTool GOLIKE Theads \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
 print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 6 \033[1;31m] \033[1;32mTool GOLIKE TIKTOK \033[1;31m[\033[1;33m termux\033[1;31m] ")
-print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 16 \033[1;31m] \033[1;32mTool GOLIKE LAZADA \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
+print("\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \033[1;32mNhập Số \033[1;31m[\033[1;33m✨ 2 \033[1;31m] \033[1;32mTool GOLIKE LAZADA \033[1;31m[\033[1;33m PC|CODESPACES|termux\033[1;31m]")
 print("\033[1;31m────────────────────────────────────────────────────────────")
 print("\033[1;37m╔═══════════════════════╗")
 print("\033[1;37m║  \033[1;33m TOOL TTC  \033[1;37m         ║")
@@ -145,10 +144,10 @@ chon = int(input('\033[1;31m[\033[1;37mBé Tập Code\033[1;31m] \033[1;37m=> \0
 #tool golike pc
 if chon == 1 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/AutoLinkedin/AutoLinkedin_PC.py').text)
-if chon == 2:
-  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/IG/IG_PC/AutoIG1_PC.py').text)
+if chon == 2 :
+  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/LAZADA-PC/lazada.py').text)
 if chon == 3 :
-  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/IG/IG_PC/AutoIG1_PC_User_Agent.py').text) 
+  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/IG/AUTOGOLIKEIG.py').text) 
 if chon == 4 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/X/AUTO-X_PC.py').text)
 elif chon == 5 : 
@@ -173,8 +172,6 @@ if chon == 14 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL%20TI%E1%BB%86N%20%C3%8DCH%20FACEBOOK/%5B21%5D%20Tool%20Reg%20Page%20Pro5.py').text)
 if chon == 15 :
   exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/regtiktok.py').text)
-if chon == 16 :
-  exec(requests.get('https://raw.githubusercontent.com/shopaccrandom/TOOLGOPHVH/refs/heads/main/TOOL_G%E1%BB%98P_HVHTOOOL/TOOL-GOLIKE/LAZADA-PC/lazada.py').text)
 if chon == 00 :
   exit()
 else :
